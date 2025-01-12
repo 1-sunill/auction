039333f70939b869ccc3fc737f74a9d4deb27369
1-sunill <pttiwarisunil46@gmail.com>
2025-01-12 17:16:53 +0530

    Add files via upload

---
+++ config/emailConfig.js
@@ -0,0 +1,15 @@
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+// console.log('dss',transporter);
+module.exports = transporter;
